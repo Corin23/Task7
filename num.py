2171f1891e936c2ef7c79e7eb89102d97979a525
Corin23 <79444354+Corin23@users.noreply.github.com>
2022-08-23 17:28:13 +0300

    Update num.py

--- num.py
+++ num.py
@@ -2,7 +2,6 @@ class ProcessNumbers:
     rep = "123"
     wth = "321"
 
-
     def __init__(self,filein,fileout,arr: list[str]):
         self.filein = filein
 
@@ -10,35 +9,23 @@ class ProcessNumbers:
 
         self.arr = arr
 
-
+        
     def readFileReplace(self):
-
         with open(self.filein , "r") as fin:
-<<<<<<< HEAD
             self.arr = [numbers.replace(ProcessNumbers.rep, ProcessNumbers.wth) for numbers in fin]
  
 
     def convertAndSort(self):
-=======
-            #for numbers in fin:
-            #   newnumber.append(numbers.replace(what, wth))
-            newnumber = [numbers.replace(rep, wth) for numbers in fin]
-        return newnumber
->>>>>>> 76ed639af94b83a6310ffe0803739768a3328a5c
-
         for i in range(0, len(self.arr)):
             self.arr[i] = int(self.arr[i])
         
         self.arr.sort()
 
-
     def writeInFile(self):
-        
         with open(self.fileout, "w") as fout:
             for items in self.arr:
                 fout.write("%i\n" % items)
 
-<<<<<<< HEAD
 
 res = []
 myProcess = ProcessNumbers("in.txt", "out.txt",res)
@@ -47,9 +34,3 @@ arr1 = myProcess.readFileReplace()
 myProcess.convertAndSort()
 
 myProcess.writeInFile()
-=======
-myProcess = ProcessNumbers("in.txt", "out.txt")
-result = myProcess.readFileReplace()
-myProcess.convertAndSort(result)
-myProcess.writeInFile(result)
->>>>>>> 76ed639af94b83a6310ffe0803739768a3328a5c
